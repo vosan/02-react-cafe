d04a2d09e40f34304cbb58d429f27f2d40d6c73f
Volodymyr Burmitskyi <vburmtsk@gmail.com>
2026-09-08 16:55:11 -0700

    feat: calculate and display vote statistics

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -2,6 +2,7 @@ import { useState } from 'react'
 import type { Votes, VoteType } from '../../types/votes'
 import CafeInfo from '../CafeInfo/CafeInfo'
 import VoteOptions from '../VoteOptions/VoteOptions'
+import VoteStats from '../VoteStats/VoteStats'
 import css from './App.module.css'
 
 function App() {
@@ -22,7 +23,11 @@ function App() {
     setVotes({ good: 0, neutral: 0, bad: 0 })
   }
 
-  const canReset = votes.good + votes.neutral + votes.bad > 0
+  const totalVotes = votes.good + votes.neutral + votes.bad
+  const positiveRate = totalVotes
+    ? Math.round((votes.good / totalVotes) * 100)
+    : 0
+  const canReset = totalVotes > 0
 
   return (
     <div className={css.app}>
@@ -31,6 +36,11 @@ function App() {
         onVote={handleVote}
         onReset={resetVotes}
         canReset={canReset}
+      />
+      <VoteStats
+        votes={votes}
+        totalVotes={totalVotes}
+        positiveRate={positiveRate}
       />
     </div>
   )
