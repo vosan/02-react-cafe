9e385019bbb75a87760f46ce06a1e82bdade0276
Volodymyr Burmitskyi <vburmtsk@gmail.com>
2026-09-08 16:54:22 -0700

    feat: add typed voting controls

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,10 +1,37 @@
+import { useState } from 'react'
+import type { Votes, VoteType } from '../../types/votes'
 import CafeInfo from '../CafeInfo/CafeInfo'
+import VoteOptions from '../VoteOptions/VoteOptions'
 import css from './App.module.css'
 
 function App() {
+  const [votes, setVotes] = useState<Votes>({
+    good: 0,
+    neutral: 0,
+    bad: 0,
+  })
+
+  const handleVote = (type: VoteType) => {
+    setVotes((currentVotes) => ({
+      ...currentVotes,
+      [type]: currentVotes[type] + 1,
+    }))
+  }
+
+  const resetVotes = () => {
+    setVotes({ good: 0, neutral: 0, bad: 0 })
+  }
+
+  const canReset = votes.good + votes.neutral + votes.bad > 0
+
   return (
     <div className={css.app}>
       <CafeInfo />
+      <VoteOptions
+        onVote={handleVote}
+        onReset={resetVotes}
+        canReset={canReset}
+      />
     </div>
   )
 }
