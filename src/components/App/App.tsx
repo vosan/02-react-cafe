57ccc77223348c7c4b3222299d7b7fec4536bba1
Volodymyr Burmitskyi <vburmtsk@gmail.com>
2026-09-08 16:52:56 -0700

    feat: add cafe information layout

---
+++ src/components/App/App.tsx
@@ -0,0 +1,12 @@
+import CafeInfo from '../CafeInfo/CafeInfo'
+import css from './App.module.css'
+
+function App() {
+  return (
+    <div className={css.app}>
+      <CafeInfo />
+    </div>
+  )
+}
+
+export default App
